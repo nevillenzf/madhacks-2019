3f5565c6cf536c852d1b99171c2fee3da31880ac
nevillenzf <nzng@wisc.edu>
2019-10-19 14:01:03 -0500

    Added ML Folder with dataset

---
+++ server/ML/model.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pathlib2
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
